4d37dca60b2cfe865902a2953054377d8d177046
carlsuarez <cjsuaarez479@gmail.com>
2024-07-13 21:51:48 -0400

    rsi calculation

--- wasm/src/fenwick_tree_implem.cpp
+++ wasm/src/fenwick_tree_implem.cpp
@@ -10,23 +10,8 @@ class fenwick_tree
 {
     std::vector<double> tree;
     std::vector<double> squareTree;
-
-    /* helper function to update fenwick tree */
-    void updateHelper(std::vector<double> &tree, int index, double val)
-    {
-        // index in tree[] is 1 more than the index in arr[]
-        index++;
-
-        // Traverse all ancestors and add val
-        while (index <= tree.size())
-        {
-            // Add val to current node
-            tree[index] += val;
-
-            // Update index to parent index
-            index += index & (-index);
-        }
-    }
+    std::vector<double> gains;
+    std::vector<double> losses;
 
     /* sum from arr[0] to arr[index] (inclusive) */
     /* O(log n), with n being the number of recorded days*/
@@ -48,17 +33,42 @@ class fenwick_tree
     {
         if (index < 0)
             return -1;
+
         return getSum(tree, index) / (index + 1);
     }
 
-    /* cumulative sum of square tree from low to high (inclusive) */
-    /* O(log n), with n being the number of recorded days*/
-    double cumulative_sum_squares(int low, int high)
+    /* Helper functions */
+
+    void updateHelper(std::vector<double> &tree, int index, double val)
+    {
+        // index in tree[] is 1 more than the index in arr[]
+        index++;
+
+        // Traverse all ancestors and add val
+        while (index <= tree.size())
+        {
+            // Add val to current node
+            tree[index] += val;
+
+            // Update index to parent index
+            index += index & (-index);
+        }
+    }
+
+    double cumulative_sum(std::vector<double> &tree, int low, int high)
     {
         if (low > 0)
-            return getSum(squareTree, high) - getSum(squareTree, low - 1);
+            return getSum(tree, high) - getSum(tree, low - 1);
         else
-            return getSum(squareTree, high);
+            return getSum(tree, high);
+    }
+
+    double interval_average(std::vector<double> &tree, int low, int high)
+    {
+        if (low > 0)
+            return getAverage(tree, high) - getAverage(tree, low - 1);
+
+        return getAverage(tree, high);
     }
 
 public:
@@ -67,12 +77,24 @@ public:
         // Fill the trees with zeros
         tree.resize(arr.size() + 1, 0);
         squareTree.resize(arr.size() + 1, 0);
+        gains.resize(arr.size() + 1, 0);
+        losses.resize(arr.size() + 1, 0);
 
         // Store the actual values in the trees using update()
         for (int i = 0; i < arr.size(); i++)
         {
             updateHelper(tree, i, arr[i]);
             updateHelper(squareTree, i, arr[i] * arr[i]);
+        }
+
+        // Calculate the changes in price and store in the corresponding vector
+        for (int i = 1; i < arr.size(); i++)
+        {
+            double delta = arr[i] - arr[i - 1];
+            if (delta > 0)
+                updateHelper(gains, i, delta);
+            else
+                updateHelper(losses, i, -delta);
         }
     }
 
@@ -94,20 +116,14 @@ public:
     /* O(log n), with n being the number of recorded days*/
     double cumulative_sum(int low, int high)
     {
-        if (low > 0)
-            return getSum(tree, high) - getSum(tree, low - 1);
-        else
-            return getSum(tree, high);
+        return cumulative_sum(tree, low, high);
     }
 
     /* average of arr[] from index low to index high */
     /* O(log n), with n being the number of recorded days*/
     double interval_average(int low, int high)
     {
-        if (low > 0)
-            return getAverage(tree, high) - getAverage(tree, low - 1);
-
-        return getAverage(tree, high);
+        return interval_average(tree, low, high);
     }
 
     /* variance of arr[] from index low to index high */
@@ -119,8 +135,8 @@ public:
             (sum(x_i * x_i) - (sum(x_i) * sum(x_i) / n)) / n
         */
         double n = (high - low + 1) * 1.0; // Size of the dataset
-        double sum = cumulative_sum(low, high);
-        double squareSum = cumulative_sum_squares(low, high);
+        double sum = cumulative_sum(tree, low, high);
+        double squareSum = cumulative_sum(squareTree, low, high);
 
         return (squareSum - (sum * sum / n)) / n;
     }
@@ -130,6 +146,20 @@ public:
     double interval_standard_deviation(int low, int high)
     {
         return std::sqrt(interval_variance(low, high));
+    }
+
+    double interval_rsi(int low, int high)
+    {
+        double gainAverage = interval_average(gains, low, high);
+        double lossAverage = interval_average(losses, low, high);
+
+        if (lossAverage == 0)
+            return 100;
+
+        double rs = gainAverage / lossAverage;
+        std::cout << "Gain: " << gainAverage << "\n";
+        std::cout << "Loss: " << lossAverage << "\n";
+        return 100 - (100 / (1 + rs));
     }
 };
 
@@ -146,22 +176,9 @@ EMSCRIPTEN_BINDINGS(fenwick_tree)
 
 int main()
 {
-    // std::vector<double> arr = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
-    // fenwick_tree fenwick(arr);
+    // std::vector<double> tree = {150.23, 152.35, 151.00, 153.45, 154.00, 152.75, 155.10, 157.25, 156.50, 158.75, 159.50, 160.00, 158.25, 160.75};
+    // fenwick_tree f(tree);
 
-    // int low = 0;
-    // int high = 5;
-    // std::cout << "Sum of arr: " << fenwick.cumulative_sum(low, high) << std::endl;
-
-    // std::cout << "Variance: " << fenwick.interval_variance(low, high) << std::endl;
-    // std::cout << "Standard Deviation: " << fenwick.interval_standard_deviation(low, high) << std::endl;
-    // std::cout << "Average: " << fenwick.interval_average(low, high) << std::endl;
-
-    // fenwick.update(2, 6.0);
-
-    // std::cout << "\nVariance after update: " << fenwick.interval_variance(low, high) << std::endl;
-    // std::cout << "Standard Deviation after update: " << fenwick.interval_standard_deviation(low, high) << std::endl;
-    // std::cout << "Average after update: " << fenwick.interval_average(low, high) << std::endl;
-
+    // std::cout << f.interval_rsi(0, tree.size() - 1) << std::endl;
     return 0;
 }
